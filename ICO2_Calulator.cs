6455df97d1fbfddf8ed8d79817f2365b51d740e6
fekri85 <fekri85@gmail.com>
2020-06-14 14:09:09 +0200

    no message

--- ICO2_Calulator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace co2_calculation
-{
-    public interface ICO2_Calulator
-    {
-        double Calc_CO2e(string tm, int d, string u = "km");
-    }
-}
--- ICO2ePresenter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace co2_calculation
-{
-    public interface ICO2ePresenter
-    {
-        void Show(double co2e_in_g, string output);
-    }
-}
